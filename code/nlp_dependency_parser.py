555867db1b164d3b666453340ec7810cebf4284f
Ruiko <love_miku@icloud.com>
2018-07-10 10:17:05 +0800

    Add dependency parser(unfinished version 2)

--- code/nlp_dependency_parser.py
+++ code/nlp_dependency_parser.py
@@ -17,36 +17,63 @@ class SpacySVOExtract(Extractor):
 		q1 = str(row['spanish_sentence1'])
 		q2 = str(row['spanish_sentence2'])
 
+		nlp = spacy.load("en")
+
 		doc1 = nlp(q1)
 		doc2 = nlp(q2)
-		for token in doc1:
-			newArc1 = DependencyArc(IndexedWord(token.head.text, token.head.i + 1), IndexedWord(token.text, token.i + 1),token.dep_)
-			arcs.add(newArc1)
-			print(newArc1)
-			print()
 
-		for token in doc2:
-			newArc2 = DependencyArc(IndexedWord(token.head.text, token.head.i + 1), IndexedWord(token.text, token.i + 1),token.dep_)
-			arcs.add(newArc2)
-			print(newArc2)
-			print()
+		feature_SVO = self.extract_SVO(doc1)
+		return feature_SVO
+
+	def extract_SVO(self, doc):
+
+		IndexedWord = namedtuple('IndexedWord', 'word index')
+		DependencyArc = namedtuple('DependencyArc', 'start_word end_word label')
+
+		arcs = set()
+		fs = list()
+
+		for token in doc:
+			newArc = DependencyArc(IndexedWord(token.head.text, token.head.i + 1), IndexedWord(token.text, token.i + 1),
+								   token.dep_)
+			arcs.add(token)
+			print(token)
+			print(token.dep_)
+			if token.dep_ == "nsubj" or token.dep_ == "ROOT" or token.dep_ == "attr":
+				temp = list()
+				temp.append(token)
+				temp.append(token.dep_)
+				fs.append(temp)
+				print(fs)
+
+			return fs
 
 
 
-nlp = spacy.load("en")
-# nlp = spacy.load("es")
 
-IndexedWord = namedtuple('IndexedWord','word index')
-DependencyArc = namedtuple('DependencyArc','start_word end_word label')
-doc = nlp(u"it is word tokenize test for spacy")
+# nlp = spacy.load("en")
+# # nlp = spacy.load("es")
+#
+# IndexedWord = namedtuple('IndexedWord','word index')
+# DependencyArc = namedtuple('DependencyArc','start_word end_word label')
+# doc = nlp(u"it is word tokenize test for spacy")
+#
+# arcs = set()
+# fs = list()
+#
+# for token in doc:
+# 	newArc = DependencyArc(IndexedWord(token.head.text,token.head.i+1),IndexedWord(token.text,token.i+1),token.dep_)
+# 	arcs.add(token)
+# 	print(token)
+# 	print(token.dep_)
+# 	if token.dep_ == "nsubj" or token.dep_ == "ROOT" or token.dep_ == "attr":
+# 		temp = list()
+# 		temp.append(token)
+# 		temp.append(token.dep_)
+# 		fs.append(temp)
+# 		print(fs)
 
-arcs = set()
-
-for token in doc:
-	newArc = DependencyArc(IndexedWord(token.head.text,token.head.i+1),IndexedWord(token.text,token.i+1),token.dep_)
-	arcs.add(newArc)
-	print(newArc)
-	print()
+	# print(arcs)
 
 
 # print(doc)
